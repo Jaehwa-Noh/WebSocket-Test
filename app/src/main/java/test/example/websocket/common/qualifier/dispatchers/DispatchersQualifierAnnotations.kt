a0ce8a3488a760d433482fdbb24c8adfec53e0a2
Jaehwa Noh <shwoghk14@gmail.com>
2025-05-16 23:11:37 +0900

    Create DispatchersQualifierAnnotations.kt

---
+++ app/src/main/java/test/example/websocket/common/qualifier/dispatchers/DispatchersQualifierAnnotations.kt
@@ -0,0 +1,12 @@
+package test.example.websocket.common.qualifier.dispatchers
+
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class DispatcherIO
+
+@Qualifier
+annotation class DispatcherDefault
+
+@Qualifier
+annotation class ApplicationScope
