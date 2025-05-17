c49f5fb87bc743ba6525a6f1dae9151624703997
Jaehwa Noh <shwoghk14@gmail.com>
2025-05-17 20:16:20 +0900

    Connect UI and websocketSession.

--- app/src/main/java/test/example/websocket/MainActivity.kt
+++ app/src/main/java/test/example/websocket/MainActivity.kt
@@ -12,37 +12,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.logging.ANDROID
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.client.plugins.websocket.webSocketSession
-import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
-import io.ktor.websocket.Frame
-import io.ktor.websocket.WebSocketSession
-import io.ktor.websocket.close
-import io.ktor.websocket.readText
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
 import test.example.websocket.ui.theme.WebSocketTestTheme
 
 @AndroidEntryPoint
@@ -67,73 +45,12 @@ fun FirstScreen(modifier: Modifier = Modifier) {
     var sendText by remember {
         mutableStateOf("")
     }
-    var getText by remember {
-        mutableStateOf("")
-    }
+    val getText by websocketViewModel.getTextStateFlow.collectAsStateWithLifecycle()
 
-    var clientWebSocket: HttpClient? by remember {
-        mutableStateOf(
-            null
-        )
-    }
-
-    var sessions: WebSocketSession? by remember {
-        mutableStateOf(
-            null
-        )
-    }
-
-    val sessionState = sessions?.isActive
-
-    val currentScope = rememberCoroutineScope()
-
-    LaunchedEffect(sessions) {
-        launch {
-            withContext(Dispatchers.IO) {
-                while (sessions?.isActive == true) {
-                    sessions?.let { websocketSessions ->
-                        websocketSessions.incoming.consumeAsFlow()
-                            .filterIsInstance<Frame.Text>()
-                            .map {
-                                getText = it.readText()
-                            }
-                            .collect()
-
-                    }
-                }
-            }
-        }
-    }
 
     Column(modifier = modifier) {
-        Button(onClick = {
-            clientWebSocket = HttpClient(CIO) {
-                install(WebSockets) {
-                    pingIntervalMillis = 20_000
-                    KotlinxWebsocketSerializationConverter(Json)
-                }
 
-                install(Logging) {
-                    logger = Logger.ANDROID
-                }
-            }
-        }) {
-            Text("Create Client")
-        }
-
-        Button(onClick = {
-            currentScope.launch {
-                sessions = clientWebSocket?.webSocketSession {
-                    url {
-                        host = "echo.websocket.org"
-                    }
-                }
-            }
-        }) {
-            Text("Create Session")
-        }
-
-        Text(getText)
+        Text(getText.message)
 
         TextField(
             value = sendText,
@@ -144,37 +61,24 @@ fun FirstScreen(modifier: Modifier = Modifier) {
 
         Button(
             onClick = {
-                currentScope.launch {
-                    sessions?.outgoing?.send(
-                        Frame.Text(sendText)
-                    )
-                }
+                websocketViewModel.sendText(sendText)
             }
         ) {
             Text("Send")
         }
 
         Button(onClick = {
-            currentScope.launch {
-                sessions?.close()
-            }
+            websocketViewModel.regenerateSession()
+        }) {
+            Text("Regenerate session")
+        }
+
+        Button(onClick = {
+            websocketViewModel.close()
         }) {
             Text("Close Session")
         }
 
-        Button(
-            onClick = {
-                clientWebSocket?.close()
-            }
-        ) {
-            Text("Close Client")
-        }
-
-        if (sessionState == true) {
-            Text("Is active")
-        } else {
-            Text("Is not Active")
-        }
     }
 }
 
